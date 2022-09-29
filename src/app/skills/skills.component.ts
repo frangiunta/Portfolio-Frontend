f0df14ca55872aeda8bf19a9b2c0a107f06be1f5
frangiunta <98899679+frangiunta@users.noreply.github.com>
2022-09-29 02:23:47 -0300

    creacion de servicio de skill

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { SkillsService } from './skills.service';
+import { Skills } from './skills';
+import { NgForm } from '@angular/forms';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-
-  constructor() { }
+public skills:Skills[]=[];
+public editSkills:Skills;
+public deleteSkills:Skills;
+public skill:Skills;
+  constructor(private skillsService:SkillsService) { }
 
   ngOnInit(): void {
+    this.getSkills();
   }
 
-}
+  public getSkills():void{
+    this.skillsService.GetSkill().subscribe
+    (
+        (response:Skills[])=>{
+          this.skills=response;
+          console.log(this.skills);}
+        ,(error:HttpErrorResponse)=>
+          {alert(error.message)}
+    );
+        }
+
+        public porcentaje(string){
+        
+          return this.skill.porcentaje;
+        }
+      }
